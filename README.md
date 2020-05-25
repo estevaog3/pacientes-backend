ef3e04c032aa84d0110d6ce2f1b5d30da203e3df
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-05-25 07:08:38 -0400

    Corrige setup-db.sql e instruções de configuração do banco de dados
    Cria o banco de dados de teste antes do script 'setup-db.sql' ser executado, o que permite criar as tabelas no banco de dados correto.
    
    Permite o usuário de teste do banco  de dados realizar consultar as tabelas criadas.

--- README.md
+++ README.md
@@ -31,19 +31,25 @@ cd labtecsMobile/backend
 sudo service postgresql start
 ```
 
-3. Conecte-se ao postgresql com um usuário que tenha privilégios para criar banco de dados no postgresql. Neste caso estou me conectando como usuário "postgres":
+3. Crie o banco de dados local de desenvolvimento e conecte-se à ele:
 
 ```
-sudo -u postgres psql
+sudo -u postgres psql -c 'CREATE DATABASE test_db'
 ```
 
-4. No prompt do `psql` que foi aberto, execute as consultas do arquivo setup_bd.sql:
+4. Conecte-se ao postgresql usando o banco de dados criado anteriormente:
+
+```
+sudo -u postgres psql -d test_db
+```
+
+5. No prompt do `psql` que foi aberto, execute as consultas do arquivo setup_bd.sql:
 
 ```
 \i ./setup_db.sql;
 ```
 
-5. Saia do psql:
+6. Saia do psql:
 
 ```
 \q
@@ -51,7 +57,7 @@ sudo -u postgres psql
 
 ##### Iniciando o servidor de backend localmente
 
-6. No diretório do backend, instale as dependencias:
+7. No diretório do backend, instale as dependencias:
 
 ```
 yarn install
@@ -63,7 +69,7 @@ ou, com npm:
 npm install
 ```
 
-7. Inicie o servidor:
+8. Inicie o servidor:
 
 ```
 yarn start
--- setup_db.sql
+++ setup_db.sql
@@ -1,19 +1,18 @@
-CREATE USER test_user WITH CREATEDB PASSWORD 'password';
-CREATE DATABASE test_db;
+CREATE USER test_user PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE "test_db" to test_user;
-CREATE TABLE dor IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS dor 
 (
   id  SERIAL PRIMARY KEY,
   intensidade INT,
   paciente_cpf CHAR(11)
 );
-CREATE TABLE desconforto IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS desconforto 
 (
   local VARCHAR(10),
   id SERIAL PRIMARY KEY,
   paciente_cpf CHAR(11)
 );
-CREATE TABLE cirurgia IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS cirurgia
 (
   id SERIAL PRIMARY KEY,
   tempo VARCHAR(10),
@@ -21,7 +20,7 @@ CREATE TABLE cirurgia IF NOT EXISTS
   sentiu_dor BOOLEAN,
   paciente_cpf CHAR(11)
 );
-CREATE TABLE pacientes IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS pacientes
 (
   cpf CHAR(11) PRIMARY KEY,
   nome VARCHAR(100),
@@ -30,7 +29,7 @@ CREATE TABLE pacientes IF NOT EXISTS
   passou_por_cirurgia BOOLEAN,
   dor_id  INT,
   desconforto_id  INT,
-  cirurgia_id  INT,
+  cirurgia_id  INT
 );
 ALTER TABLE dor ADD FOREIGN KEY (paciente_cpf) REFERENCES pacientes(cpf);
 ALTER TABLE desconforto ADD FOREIGN KEY (paciente_cpf) REFERENCES pacientes(cpf);
@@ -38,3 +37,4 @@ ALTER TABLE cirurgia ADD FOREIGN KEY (paciente_cpf) REFERENCES pacientes(cpf);
 ALTER TABLE pacientes ADD FOREIGN KEY (dor_id) REFERENCES dor(id);
 ALTER TABLE pacientes ADD FOREIGN KEY (desconforto_id) REFERENCES desconforto(id);
 ALTER TABLE pacientes ADD FOREIGN KEY (cirurgia_id) REFERENCES cirurgia(id);
+GRANT ALL PRIVILEGES ON TABLE pacientes, dor, desconforto, cirurgia TO test_user;
